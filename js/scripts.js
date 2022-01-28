87e42f9ad3de071b59e76795efb2789244e74874
mizzucho <97363960+mizzucho@users.noreply.github.com>
2022-01-27 18:15:13 -0600

    2.3:Errpr
    the hamburger sign would open on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 
 console.log("Welcome to my portfolio site!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
